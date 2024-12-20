6816c71b667d7b309079892786e0b86e2e3a4c22
hsilnam <hsilnam3@gmail.com>
2024-12-21 06:42:16 +0900

    Init: 기본 프로젝트 생성, gitignore 설정
    - gitignore: intellj, 예민한 앱 설정 파일 무시

---
+++ src/main/java/com/sparta/quickreserveproject/QuickReserveProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.quickreserveproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickReserveProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickReserveProjectApplication.class, args);
+    }
+
+}
